642551ecdc58a60bf16c26092a87b692d7c3b8b1
Joshua Frazer <77747282+BlackOutDevelops@users.noreply.github.com>
2023-04-10 16:11:22 -0400

    Add navbar components and page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    HobbiesComponent,
+    WorkComponent,
+    ContactMeComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', component: HomeComponent },
+      { path: 'hobbies', component: HobbiesComponent },
+      { path: 'work', component: WorkComponent },
+      { path: 'contact-me', component: ContactMeComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact-me/contact-me.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-me',
+  templateUrl: 'contact-me.component.html',
+  styleUrls: ['contact-me.component.css']
+})
+export class ContactMeComponent { }
