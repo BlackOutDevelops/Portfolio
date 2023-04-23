e22bbb75f7ec5957842269e04d2d1e60a41b7fb4
Joshua Frazer <joshiefrazie@hotmail.com>
2023-04-23 03:27:10 -0400

    Update Contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 
 import { AppComponent } from './app.component';
@@ -13,6 +16,7 @@ import { ContactMeComponent } from './contact-me/contact-me.component';
 import { HobbiesComponent } from './hobbies/hobbies.component';
 import { HomeComponent } from './home/home.component';
 import { WorkComponent } from './work/work.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -29,12 +33,17 @@ import { WorkComponent } from './work/work.component';
     MatButtonModule,
     MatIconModule,
     MatGridListModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     RouterModule.forRoot([
       { path: '', pathMatch: 'full', component: HomeComponent },
       { path: 'hobbies', component: HobbiesComponent },
       { path: 'work', component: WorkComponent },
       { path: 'contact-me', component: ContactMeComponent }
     ], { scrollPositionRestoration: 'enabled' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'contact-me',
   templateUrl: 'contact-me.component.html',
   styleUrls: ['contact-me.component.css']
 })
-export class ContactMeComponent { }
+export class ContactMeComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  sendEmail() {
+    //const link = document.createElement('a');
+    //link.setAttribute('target', '_blank');
+    //link.setAttribute('href', 'mailto: joshiefrazie@hotmail.com ? Subject=Feedback');
+    //document.body.appendChild(link);
+    //link.click();
+    //link.remove();
+  }
+}
