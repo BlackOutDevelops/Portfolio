7fca97c018f98e6397167e263924be8a51316be9
Joshua Frazer <77747282+BlackOutDevelops@users.noreply.github.com>
2023-04-25 10:52:02 -0400

    Update contact component

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,28 +1,98 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgForm, NgModel, Validators } from '@angular/forms';
+import { MatInput } from '@angular/material/input';
+import { Event } from '@angular/router';
 
 @Component({
   selector: 'contact-me',
   templateUrl: 'contact-me.component.html',
   styleUrls: ['contact-me.component.css']
 })
-export class ContactMeComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
+export class ContactMeComponent implements OnInit{
+  //emailValidation = new FormControl('', [Validators.required, Validators.email]);
+  secretKey: string = "mqkowdyz";
+  emailForm!: FormGroup;
+  phone: string = '';
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
+  constructor(private fb: FormBuilder, private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.emailForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}-[0-9]{3}-[0-9]{4}')]),
+      message: new FormControl('')
+    })
+  }
+  
+  getEmailErrorMessage() {
+    if (this.emailForm.controls['email'].hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.emailForm.controls['email'].hasError('email') ? 'Not a valid email' : '';
   }
 
-  sendEmail() {
-    //const link = document.createElement('a');
-    //link.setAttribute('target', '_blank');
-    //link.setAttribute('href', 'mailto: joshiefrazie@hotmail.com ? Subject=Feedback');
-    //document.body.appendChild(link);
-    //link.click();
-    //link.remove();
+  getPhoneErrorMessage() {
+    if (this.emailForm.controls['phone'].hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.emailForm.controls['phone'].hasError('pattern') ? 'Not a valid phone number' : '';
+  }
+
+  getMessageErrorMessage() {
+    return this.emailForm.controls['message'].hasError('required') ? 'You must enter a value' : '';
+  }
+
+  onDelete(event: any) {
+    let inputElement = (event.target as HTMLInputElement);
+    let pos = inputElement.selectionStart;
+    setTimeout(() => inputElement.selectionStart = inputElement.selectionEnd = pos, .5);
+  }
+
+  addHyphen() {
+    for (let i = 0; i < this.phone.length; i++) {
+      if (i == 3)
+        this.phone = this.phone.slice(0, i) + '-' + this.phone.slice(i, this.phone.length);
+      else if (i == 7)
+        this.phone = this.phone.slice(0, i) + '-' + this.phone.slice(i, this.phone.length);
+      else if (this.phone[i] == '-')
+        this.phone = this.phone.slice(0, i) + '' + this.phone.slice(i + 1, this.phone.length);
+    }
+  }
+
+  onSubmit() {
+    /*Set the url with your secretKey from formspree.io*/
+    let url = "https://formspree.io/f/" + this.secretKey;
+    /*Set Headers*/
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      })
+    };
+
+    let data = `email=${this.emailForm.value.email}&message=${this.emailForm.value.message}\n\nCall me at: ${this.emailForm.value.phone}`;
+    let errorMessage: string = "";
+
+    console.log(this.emailForm.value);
+
+    if (this.emailForm.valid) {
+      this.httpClient.post<any>(url, data, httpOptions).subscribe({
+        next: data => {
+          console.log("email sent" + JSON.stringify(data));
+        },
+        error: error => {
+          errorMessage = error.message;
+          console.log('error!', errorMessage);
+        }
+      })
+      console.log("Form Submitted!\nMessage sent:\n" + data);
+      this.emailForm.reset();
+    }
+    else {
+      console.log("Form Not Submitted!\nPlease Fill All Fields!");
+    }
   }
 }
