e5488770c72165f302a30d32bc39fa18e5547920
Joshua Frazer <77747282+BlackOutDevelops@users.noreply.github.com>
2023-04-30 14:20:04 -0400

    Add percentage bar logic to highlight sections of the percentage bar when a language chip is clicked.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { GithubService } from '../app/services/github.service';
 
-
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ElementRef, Input, OnInit, QueryList, ViewChild } from '@angular/core';
+import { elementAt } from 'rxjs';
+import { TimerHandle } from 'rxjs/internal/scheduler/timerHandle';
 
 @Component({
   selector: 'project-card',
@@ -7,4 +9,45 @@ import { Component, Input } from '@angular/core';
 })
 export class ProjectCardComponent {
   @Input() project: any;
+  @ViewChild('percentageBar')
+  percentageBar!: ElementRef;
+  timer!: TimerHandle;
+
+  glowBar(color: string) {
+    const percentageBar = this.percentageBar.nativeElement as HTMLElement;
+    const rgbColor = this.hexToRgb(color);
+
+    percentageBar.childNodes.forEach(element => {
+
+      if (element.nodeName == 'DIV') {
+        let barPiece = element as HTMLDivElement;
+
+        barPiece.childNodes.forEach(element => {
+
+          if (element.nodeName == 'SPAN') {
+            let colorBar = element as HTMLSpanElement;
+
+            if (colorBar.style.boxShadow != 'unset')
+              colorBar.style.boxShadow = 'unset';
+
+            if (colorBar.style.backgroundColor == rgbColor) {
+              colorBar.style.boxShadow = '0px 0px 20px 5px ' + rgbColor;
+
+              clearTimeout(this.timer);
+              this.timer = setTimeout(() => {
+                if (colorBar.style.boxShadow != 'unset')
+                  colorBar.style.boxShadow = 'unset';
+              }, 3000);
+            }
+          }
+        });
+      }
+    });
+  }
+
+
+  hexToRgb(hex: string): string {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? 'rgb(' + [parseInt(result[1], 16).toString(), parseInt(result[2], 16).toString(), parseInt(result[3], 16).toString()].join(', ') + ')' : '';
+  }
 }
