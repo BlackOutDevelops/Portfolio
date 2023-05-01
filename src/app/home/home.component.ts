89c580214d09bb6cf3cafed60ffe9af2504da1bc
Joshua Frazer <77747282+BlackOutDevelops@users.noreply.github.com>
2023-05-01 13:09:52 -0400

    Add favorite projects carousel to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, Directive, HostListener, ViewEncapsulation } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'home',
@@ -6,11 +7,14 @@ import { Component, Directive, HostListener, ViewEncapsulation } from '@angular/
   styleUrls: ['home.component.css'],
 })
 export class HomeComponent {
+  favProjects = this.dataService.favoriteProjects;
   clickMessage: string = "";
   programmingLanguages: Array<string> = ['C', 'Java', 'SQL', 'Python', 'C++', 'C#', 'HTML', 'CSS', 'Javascript', 'Kotlin for Android'];
   frameworkExperience: Array<string> = ['Angular.js', 'React.js', 'WPF', 'WinForms', '.NET Framework', '.NET Core'];
   proficientTools: Array<string> = ['Bash', 'Eclipse', 'Code Composer', 'Visual Studio', 'Atom', 'Intellij IDEA', 'Windows', 'Ubuntu', 'MobaXterm', 'TortoiseSVN', 'SVN', 'GIT',
     'Github', 'Android Studio', 'Azure DevOps'];
+
+  constructor(private dataService: DataService) { }
 
   downloadResume() {
     this.clickMessage = "Downloading Resume...";
